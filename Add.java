cdaeb4ff5c18e21b42c66ee42d423f034f8e59f0
170030528 <59111209+170030528@users.noreply.github.com>
2020-01-04 15:02:09 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -3,9 +3,9 @@ class Add
 {
 public static void main(String[] args)
 {
-int a=5;
+int a=50;
 int b=10;
 int c=b+a;
-System.out.println("sum"+c);
+System.out.println("sum of"+a+"and"+b+"is"+c);
 }
 }
